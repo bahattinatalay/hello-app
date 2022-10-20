62e803e6c936f22f7d5c142764602b10876a20d0
adrian <adrianxyz999@gmail.com>
2022-10-20 21:42:55 +0200

    replicas updated

--- welcome.py
+++ welcome.py
@@ -2,7 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "<h1> Welcome to your new office! This image will be sent to docker hub! </h1> "
-    "<h1> Deployed on Kubernetes on AWS. Version 5.1.0 </h1> "
+    return "<h1> Welcome to your new office! This image will be sent to docker hub! Deployed on Kubernetes on AWS. Version 5.1.0 </h1> "
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
